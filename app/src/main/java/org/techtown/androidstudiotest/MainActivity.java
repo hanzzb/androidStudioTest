1680042066b15b5646a8fa2890ec8e9599e540a2
hanzzb <hellojione2@gmail.com>
2020-09-24 23:22:17 +0900

    branchTest4

--- app/src/main/java/org/techtown/androidstudiotest/MainActivity.java
+++ app/src/main/java/org/techtown/androidstudiotest/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     int branchTest;
     int branchTest2;
     int branchTest3;
+    int branchTest4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
